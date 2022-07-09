b805eaf1c4c4df496d72c185463a015e55e096f5
Lion Hofman <lion.hofman@posthaus.com.br>
2022-07-09 09:37:32 -0300

    ✨feat: initializing the database config

---
+++ lib/helpers/contact.dart
@@ -0,0 +1,44 @@
+// Tabela com id, name, email,                phone,    img
+//            5454 Lion lionhofman@gmail.com  333333    /images/
+import 'package:contact_book/helpers/contact_helper.dart';
+
+class Contact {
+  int? id;
+  String? name;
+  String? email;
+  String? phone;
+  String? img;
+
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img,
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  Contact({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.img,
+  });
+
+  @override
+  String toString() {
+    return "Contact(id:$id, name: $name, email:$email, phone:$phone, img: $img)";
+  }
+}
